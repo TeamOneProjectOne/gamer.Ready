90a905a43e68df51a7f98d4428c0d588f7fa4e0b
Amazonian32 <ynitsedekralc@gmail.com>
2020-01-25 12:39:17 -0700

    added firebase; added twitch api; added some styles; added fonts; got the search functions started;
    UI/UX are responding well right now. We are going to work on more api and UI

--- assets/Javascript/api.js
+++ assets/Javascript/api.js
@@ -1,24 +1,67 @@
-// var apiKEY = "da8bc7eb09028fc87c2de72e2cc90027"
-var queryURL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added"
-var gamesData = [];
 
 
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    for (let i = 0; i < response.results.length; i++) {
-        var rawgResults = $("<div>").attr("id", "2019games");
-        var rawgImages = $("<img>").attr("data-imgRAWG", "bGround");
-        var rawgRatings = $("<div>").attr("id", "rating");
-        rawgResults.text(response.results[i].name);
-        rawgRatings.text("Rating: " + response.results[i].rating);
-        rawgImages.attr("src", response.results[i].short_screenshots[0].image);
-        $("#test").prepend(rawgResults, rawgRatings, rawgImages);
+$(document).ready(function () {
+
+    function rawg() {
+        var queryURL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added"
+        var gamesData = [];
+
+        // }
+
+        // 
+
+        // function twitch(){
+        //     var queryURL = "https://api.twitch.tv/helix/?client-id=mf1yqwkv1gtvjwizwhg7vnlkcbhbvd"
+        // }
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            for (let i = 0; i < response.results.length; i++) {
+                var rawgResults = $('<div class="thisDiv">').attr("id", "2019games");
+                var rawgImages = $("<img>").attr("data-imgRAWG", "bGround");
+                rawgResults.append(rawgImages)
+                var rawgRatings = $("<div>").attr("id", "rating");
+                rawgResults.append(rawgRatings)
+                rawgResults.append(response.results[i].name);
+                rawgRatings.text("Rating: " + response.results[i].rating);
+                rawgImages.attr("src", response.results[i].short_screenshots[0].image);
+                $("#test").prepend(rawgResults);
+            }
+
+
+        })
     }
-    
+    function twitch(){
+        var apiKEY = "mf1yqwkv1gtvjwizwhg7vnlkcbhbvd"
+        var queryURL = "https://api.twitch.tv/helix/streams?game_id=33214?Client-ID=" + apiKEY;
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response){
+            console.log(response);
+        })
 
-})
+    }
+        rawg();
+        twitch();
 
+        $("#searchButton").on("click", function (event) {
+            event.preventDefault();
+            var searchInputGame = $("#searchInput").val().trim();
+            $("#test").empty();
+            var queryUrl = "https://api.rawg.io/api/" + searchInputGame;
+            console.log("because clicked: " + newQuery)
+        });
+
+
+        $(".thisDiv").on("click", function (event) {
+            // event.preventDefault();
+            console.log("clicky-div")
+            // var thisGame = $(this).attr("<a ")
+        });
+    });
