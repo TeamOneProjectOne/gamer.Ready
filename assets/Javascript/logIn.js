d7b89b766a04ab95bc4146a6f580e87e60e723cf
Jeff.A <kinesthesiac@gmail.com>
2020-01-27 16:57:05 -0700

    added pages: logIn.html/js,  register.html/js, gamePage.html/js.  renamed userHome as myGames.  Added copy to getting started.  Wired the links on each page(though a few remain to be done). Fought with the submit button on the logIn page for 25 minutes. XD

---
+++ assets/Javascript/logIn.js
@@ -0,0 +1,33 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyATyfvW4ljW1OPganzWkNMXK7F_nMgzQPQ",
+  authDomain: "fir-demo-33d93.firebaseapp.com",
+  databaseURL: "https://fir-demo-33d93.firebaseio.com",
+  projectId: "fir-demo-33d93",
+  storageBucket: "fir-demo-33d93.appspot.com",
+  messagingSenderId: "987132343245",
+  appId: "1:987132343245:web:b050e90e84c0b398c38a84",
+  measurementId: "G-MP2NW7NGV7"
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var database = firebase.database();
+
+var inputUsername = "";
+
+$("#logInButton").on("click", function (event) {
+  event.preventDefault();
+  console.log("clicked logInButton")
+
+//   inputUsername = $("#inlineFormInputGroupUsername").val().trim();
+//   console.log(userEmail)
+//   console.log(inputName)
+//   console.log(inputUsername)
+
+  
+//   $(location).attr('href', "myGames.html");
+
+});
+
