2997f2b6f3f44f2ae27be1dd1685dca386fb2122
Amazonian32 <ynitsedekralc@gmail.com>
2020-01-27 21:53:17 -0700

    edited a couple of things; added picture and rating to the game page.

--- assets/Javascript/gamePage.js
+++ assets/Javascript/gamePage.js
@@ -31,7 +31,9 @@ $(document).ready(function () {
             var rawgResults = $('<div class="thisGame">').attr("id", "thisGames");
             var rawgImages = $("<img>").attr("data-imgThisGame", "bGround");
             rawgImages.attr("src", response.results[0].background_image)
-            $("#thisGame").append(rawgImages);
+            var rawgRatings = $('<div id="thisGameRating"></div>')
+            rawgRatings.text(response.results[0].rating)
+            $("#thisGame").append(rawgImages, rawgRatings);
 
 
         });
@@ -56,6 +58,7 @@ $(document).ready(function () {
                     var videoImage = $("<img>")
                     videoImage.attr("src", $(item.snippet.thumbnails.medium.url));
                     videoResults.prepend(videoImage);
+
                     
     
                     $("#videotest").prepend(videoResults);
