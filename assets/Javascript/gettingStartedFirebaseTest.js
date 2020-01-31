eb0a63e5f6bdf3fa5108d2c463cdcb86759a70f2
Jeff.A <kinesthesiac@gmail.com>
2020-01-31 14:53:22 -0700

    Struggling with firbase authentication... teensy tiny progress

--- assets/Javascript/gettingStartedFirebaseTest.js
+++ assets/Javascript/gettingStartedFirebaseTest.js
@@ -12,52 +12,76 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+errorCode = "";
+errorMessage = "";
+
 
 //email/password login
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+// firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//   });
 
-  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+//   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // ...
+//   });
 
-  firebase.auth().signOut().then(function() {
-    // Sign-out successful.
-  }).catch(function(error) {
-    // An error happened.
-  });
+//   firebase.auth().signOut().then(function() {
+//     // Sign-out successful.
+//   }).catch(function(error) {
+//     // An error happened.
+//   });
 
 var database = firebase.database();
 
 var userEmail = "";
+var userPassword = "";
 var inputName = "";
 var inputUsername = "";
 
 $("#getStartedButton").on("click", function (event) {
-  event.preventDefault();
+  
   console.log("clicked submit")
-
+  
   userEmail = $("#inputEmail1").val().trim();
+  userPassword = $("#inputPassword6").val().trim();
   inputName = $("#inlineFormInputName").val().trim();
   inputUsername = $("#inlineFormInputGroupUsername").val().trim();
-  console.log(userEmail)
-  console.log(inputName)
-  console.log(inputUsername)
 
-  database.ref().push({
-    userEmailDB: userEmail,
-    inputNameDB: inputName,
-    inputUsernameDB: inputUsername
-  });
-  
-  $(location).attr('href', "userHome.html");
+
+  if (userPassword.length >=8 && userPassword.length <= 20) {
+    event.preventDefault();
+    
+    console.log(userEmail)
+    console.log(userPassword)
+    console.log(inputName)
+    console.log(inputUsername)
+    
+    // database.ref().push({
+    //   userEmailDB: userEmail,
+    //   userPasswordDB: userPassword,
+    //   inputNameDB: inputName,
+    //   inputUsernameDB: inputUsername
+    // });
+    
+    // $(location).attr('href', "myGames.html");
+    
+  } else {
+
+    //triggers modal:
+    $('#passwordModal').on('shown.bs.modal', function () {
+      $('#passwordModal').trigger('focus')
+
+    })
+
+  }
+
+
 
 });
 
