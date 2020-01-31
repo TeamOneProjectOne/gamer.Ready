f8b0407a0c298831d40884213a81e82924047b94
Jeff.A <kinesthesiac@gmail.com>
2020-01-31 00:10:47 -0700

    Created test versions of gettingStarted.html and .js and attempted to code the signup and login authentication structure.  Not yet but I'll keep trying!

---
+++ assets/Javascript/gettingStartedFirebaseTest.js
@@ -0,0 +1,63 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC_QkERVePK958PQ5mNF4CpsbJpXoi9MCc",
+  authDomain: "gamerdotready.firebaseapp.com",
+  databaseURL: "https://gamerdotready.firebaseio.com",
+  projectId: "gamerdotready",
+  storageBucket: "gamerdotready.appspot.com",
+  messagingSenderId: "24970463108",
+  appId: "1:24970463108:web:2d43535809d85ea1943fd1",
+  measurementId: "G-SRQNTP3P9G"
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+//email/password login
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+  }).catch(function(error) {
+    // An error happened.
+  });
+
+var database = firebase.database();
+
+var userEmail = "";
+var inputName = "";
+var inputUsername = "";
+
+$("#getStartedButton").on("click", function (event) {
+  event.preventDefault();
+  console.log("clicked submit")
+
+  userEmail = $("#inputEmail1").val().trim();
+  inputName = $("#inlineFormInputName").val().trim();
+  inputUsername = $("#inlineFormInputGroupUsername").val().trim();
+  console.log(userEmail)
+  console.log(inputName)
+  console.log(inputUsername)
+
+  database.ref().push({
+    userEmailDB: userEmail,
+    inputNameDB: inputName,
+    inputUsernameDB: inputUsername
+  });
+  
+  $(location).attr('href', "userHome.html");
+
+});
+
